df801709b72a8816ac34849c33e6baade9b20c74
SylvainPS78 <186784532+SylvainPS78@users.noreply.github.com>
2025-04-16 15:25:02 +0700

    FEAT ADD desktop design

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ const slider = {
   wrapper: document.querySelector(".slider__wrapper"),
   slides: document.querySelectorAll(".slider__slide"),
   totalSlides: document.querySelectorAll(".slider__slide").length,
+  mobileImages: [
+    "./images/mobile-image-hero-1.jpg",
+    "./images/mobile-image-hero-2.jpg",
+    "./images/mobile-image-hero-3.jpg",
+  ],
+  desktopImages: [
+    "./images/desktop-image-hero-1.jpg",
+    "./images/desktop-image-hero-2.jpg",
+    "./images/desktop-image-hero-3.jpg",
+  ],
+
+  mediaQuery: window.matchMedia("(min-width: 769px)"),
 
   init() {
     document
@@ -11,6 +23,25 @@ const slider = {
     document
       .querySelector(".slider__button--next")
       .addEventListener("click", () => this.nextSlide());
+
+    this.handleImagesSrc();
+    this.mediaQuery.addEventListener("change", () => this.handleImagesSrc());
+    window.addEventListener("resize", () => this.handleImagesSrc());
+  },
+
+  handleImagesSrc() {
+    const isDesktop = this.mediaQuery.matches;
+    const images = document.querySelectorAll(".slider__image");
+
+    images.forEach((img, index) => {
+      const newSrc = isDesktop
+        ? this.desktopImages[index]
+        : this.mobileImages[index];
+
+      if (img.src !== newSrc) {
+        img.src = newSrc;
+      }
+    });
   },
 
   goToSlide(index) {
@@ -34,12 +65,15 @@ const navigation = {
   overlay: document.querySelector(".overlay"),
   openButton: document.querySelector(".navbar-open-button"),
   closeButton: document.querySelector(".navbar-close-button"),
+  mediaQuery: window.matchMedia("(min-width: 769px)"),
   isOpen: false,
 
   init() {
     this.openButton.addEventListener("click", () => this.open());
     this.closeButton.addEventListener("click", () => this.close());
     this.overlay.addEventListener("click", () => this.close());
+    this.handleInert();
+    this.mediaQuery.addEventListener("change", () => this.handleInert());
   },
 
   open() {
@@ -60,6 +94,20 @@ const navigation = {
     this.openButton.setAttribute("aria-expanded", "false");
     this.isOpen = false;
     document.body.style.overflow = "";
+  },
+
+  handleInert() {
+    if (this.mediaQuery.matches) {
+      this.navbar.removeAttribute("inert");
+      this.navbar.style.opacity = "1";
+    } else {
+      this.navbar.setAttribute("inert", "");
+      this.navbar.style.opacity = "0";
+      this.overlay.classList.remove("overlay--active");
+      this.overlay.setAttribute("inert", "");
+      this.isOpen = false;
+      document.body.style.overflow = "";
+    }
   },
 };
 
